f19ae33e8a0aa2fa6e2fb6cc0235c04d205111a7
William Xu <79387306+WilliamXu50@users.noreply.github.com>
2021-10-14 21:56:15 -0400

    Simplified the main game loop

--- src/main.c
+++ src/main.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <pthread.h>
 
 #include <SDL2/SDL.h>
 #include "machine.h"
 #include "input.h"
 #include "graphics.h"
-
-/*void* get_key_input(void* arg){
-    machine_t* machine=(machine_t*)arg;
-
-    while(machine->quit_status!=1){
-        keyboard_handler(machine);
-    }
-
-    pthread_detach(pthread_self());
-    return NULL;
-}*/
 
 int main(){
     display_t* game_display=malloc(sizeof(display_t));
@@ -25,43 +13,43 @@ int main(){
 
     machine_t* machine=init_machine();
 
-    //Load Space Invader Rom files into memory
+    //Load Space Invader ROM files into memory
     load_game(machine);
 
     int time=SDL_GetTicks();
 
     while(machine->quit_status!=1){
-        //Get user input
-        keyboard_handler(machine);
-
-        //Every 17 ms (60 fps)
+        //Every 17 ms a new frame updates (very roughly 60 fps)
         if((SDL_GetTicks() - time) > (1.0f / FPS) * 1000){
+            //Update elapsed time
             time=SDL_GetTicks();
+            
+            //Get user input
+            keyboard_handler(machine);
 
-            int cycles=0;   //Total number of instruction cycles
+            int total_cycles=0;   //Total number of instruction cycles
 
-            while(cycles<=CYCLES_PER_FRAME){
-                int current_cycle=machine->cpu->instruction_cycles;
+            int current_cycle=0;    /*machine->cpu->instruction_cycles-current_cycle
+                                      would give the CPU cycles after executing an instruction*/
 
+            while(total_cycles<=HALF_CYCLES_PER_FRAME){
+                current_cycle=machine->cpu->instruction_cycles;
                 machine_execute(machine);
-
-                cycles+=machine->cpu->instruction_cycles-current_cycle;
-
-                if(machine->cpu->instruction_cycles>=HALF_CYCLES_PER_FRAME){
-                    //Only generate interrupt if interrupt-enable is set
-                    if(machine->cpu->interrupt_enable==1){
-                        //Disable interrupt enable
-                        machine->cpu->interrupt_enable=0;
-                        //Generate the interrupt with the appropriate interrupt number
-                        RST(machine->cpu, machine->int_num);
-                        //11 cycles taken for interrupts
-                        machine->cpu->instruction_cycles+=11;
-                    }
-                    machine->cpu->instruction_cycles-=HALF_CYCLES_PER_FRAME;
-
-                    machine->int_num=(machine->int_num==1 ? 2 : 1);
-                }
+                total_cycles+=machine->cpu->instruction_cycles-current_cycle;
             }
+            //Generate mid-screen interrupt (interrupt number = 1)
+            generate_interrupt(machine, 1);
+
+            current_cycle=0;
+
+            while(total_cycles<=CYCLES_PER_FRAME){
+                current_cycle=machine->cpu->instruction_cycles;
+                machine_execute(machine);
+                total_cycles+=machine->cpu->instruction_cycles-current_cycle;
+            }
+            //Generate end-of-screen interrupt (interrupt number = 2)
+            generate_interrupt(machine, 2);
+            
             machine_update_screen(machine);
             render_graphics(game_display, machine);
         }
